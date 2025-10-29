7dea07922810b0a1458712b522d8cbf2093e88f4
abdulazizkhatamov <abdulazizkhatamov@hotmail.com>
2025-10-29 16:17:09 +0500

    cart middleware

--- apps/main-server/src/middleware/cart.middleware.ts
+++ apps/main-server/src/middleware/cart.middleware.ts
@@ -7,15 +7,18 @@ export class CartIdMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const cookies = req.cookies as Record<string, string | undefined>;
     let cartId: string | undefined = cookies['cartId'];
+
     if (!cartId) {
       cartId = uuid();
       res.cookie('cartId', cartId, {
         httpOnly: true,
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        sameSite: 'lax',
+        secure: true, // ✅ required for SameSite: 'none'
+        sameSite: 'none', // ✅ allow sending across sites (vercel <-> render)
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       });
     }
 
+    // Attach to request for later use
     req.cartId = cartId;
     next();
   }
