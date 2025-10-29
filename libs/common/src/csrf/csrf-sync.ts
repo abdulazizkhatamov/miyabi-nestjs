e65e4af3e8f824150f3579a74f4cb8d2bc3fb2e0
abdulazizkhatamov <abdulazizkhatamov@hotmail.com>
2025-10-29 17:57:33 +0500

    getToken debugging

--- libs/common/src/csrf/csrf-sync.ts
+++ libs/common/src/csrf/csrf-sync.ts
@@ -4,9 +4,14 @@ import { csrfSync } from 'csrf-sync';
 // Use default session key (_csrfSecret) and read token from x-csrf-token header
 const { generateToken, csrfSynchronisedProtection } = csrfSync({
   getTokenFromRequest: (req) => {
-    const csrfToken = req.session.csrfToken;
-    console.log(`Got token from server state: ${csrfToken}`);
-    return csrfToken;
+    const token = req.headers['x-csrf-token'] as string;
+    console.log(`getTokenFromRequest token: ${token}`);
+    return token;
+  },
+  getTokenFromState: (req) => {
+    const token = req.session.csrfToken;
+    console.log(`getTokenFromState token: ${token}`);
+    return token;
   },
 });
 
